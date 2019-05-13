aa2dcb1c6cd13753b30ca83f962dba83ecd1fb5c
DROUAUD Aurélien <a.drouaud@gmail.com>
2019-05-13 11:40:41 +0200

    Ajout du détail (non fonctionnel) et de la partie commentaire (affichage marche, pas la fonction)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,12 @@ import { Routes } from '@angular/router';
 import { GallerieComponentComponent } from './gallerie-component/gallerie-component.component';
 import { AproposComponentComponent } from './apropos-component/apropos-component.component';
 import { HomeComponentComponent } from './home-component/home-component.component';
-import { DetailCollegueComponentComponent } from './detail-collegue-component/detail-collegue-component.component';
-
 
 export const ROUTES: Routes = [
     { path: 'home', component: HomeComponentComponent },
     { path: 'gallerie', component: GallerieComponentComponent },
+    { path: 'gallerie', component: GallerieComponentComponent },
     { path: 'about', component: AproposComponentComponent },
-    { path: 'galerie/:matricule', component: DetailCollegueComponentComponent},
 
     { path: '', pathMatch: 'full', redirectTo: '/home' },
 ];
